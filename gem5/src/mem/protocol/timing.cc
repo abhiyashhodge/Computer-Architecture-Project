90036814f0fb62f33f28af7081862c5e1ae4ad85
Matan Broner <matanbroner@gmail.com>
2022-04-27 14:33:24 -0500

    log pkt fix

--- gem5/src/mem/protocol/timing.cc
+++ gem5/src/mem/protocol/timing.cc
@@ -48,7 +48,7 @@ namespace gem5
 bool
 TimingRequestProtocol::sendReq(TimingResponseProtocol *peer, PacketPtr pkt)
 {
-    pkt->print(stdout);
+    pkt->print();
     assert(pkt->isRequest());
     return peer->recvTimingReq(pkt);
 }
